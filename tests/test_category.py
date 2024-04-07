17917aba8a41ff2f8bd6956b2aad93a2aa312ec7
temirkan <tboziev@gmail.com>
2024-04-07 21:20:58 +0300

    ok+

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,12 @@
 import pytest
-from src.category import Category
+from src.classes_package.category import Category
 
 
-def test_init(null, for_category):
+def test_init(null, for_category, product_0_quant):
     with pytest.raises(TypeError):
         Category('лол', 'дважды лол', ['какой-то', 'список'])
+    with pytest.raises(TypeError):
+        Category('лол2', 'дважды лол2', [product_0_quant])
     assert for_category.name == "Смартфоны"
     assert for_category.description == ("Смартфоны, как средство не только "
                                         "коммуникации, но и "
@@ -16,12 +18,19 @@ def test_init(null, for_category):
     assert Category.products_count == 2
 
 
-def test_add_product(null, for_category, for_product_1):
+def test_add_product(null, for_category, for_product_1, product_0_quant):
     for_category.add_product(for_product_1)
     assert Category.products_count == 3
-    assert for_category.products[2].startswith("Xiaomi Redmi Note 11") is True
+    assert for_category.products[-1].startswith("Xiaomi Redmi Note 11") is True
+    with pytest.raises(ValueError):
+        for_category.add_product(product_0_quant)
     with pytest.raises(TypeError):
         for_category.add_product('лол')
+
+
+def test_avg_price(for_category, empty_category):
+    assert for_category.avg_price() == 195000.0
+    assert empty_category.avg_price() == 0
 
 
 def test_product_getter(for_category):
